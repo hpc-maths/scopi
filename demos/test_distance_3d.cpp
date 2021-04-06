a557424f32fc4c7d447d1fe082b0db067347a60a
gouarin <loic.gouarin@gmail.com>
2021-04-06 14:45:48 +0200

    add distance 3d example

---
+++ demos/test_distance_3d.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <xtensor/xfixed.hpp>
+#include <xtensor/xio.hpp>
+
+#include <scopi/container.hpp>
+#include <scopi/object/sphere.hpp>
+#include <scopi/object/plan.hpp>
+
+#include <scopi/functors.hpp>
+#include <scopi/types.hpp>
+
+int main()
+{
+    constexpr std::size_t dim = 3;
+    double theta = 4*std::atan(1.)/4;
+
+    scopi::type::rotation<dim> rotation{{{std::cos(theta), -std::sin(theta), 0},
+                                         {std::sin(theta),  std::cos(theta), 0},
+                                         {              0,                0, 1}}};
+
+    scopi::type::position<dim> translation = {0, 0, 0};
+
+    auto s1_pos = xt::eval(translation + 0.5*xt::view(rotation, xt::all(), 0));
+    auto s2_pos = xt::eval(translation - 0.5*xt::view(rotation, xt::all(), 0));
+
+    scopi::sphere<dim> s1({s1_pos}, 0.4);
+    scopi::sphere<dim> s2({s2_pos}, 0.4);
+    scopi::plan<dim> p1({translation}, theta);
+
+    scopi::scopi_container<dim> particles;
+
+    scopi::type::position<dim> dummy = {0, 0, 0};
+
+    particles.push_back(s1, {dummy}, {dummy}, {dummy});
+    particles.push_back(s2, {dummy}, {dummy}, {dummy});
+    particles.push_back(p1, {dummy}, {dummy}, {dummy});
+
+    for(std::size_t i = 0; i < particles.size() - 1; ++i)
+    {
+        for(std::size_t j = i + 1; j < particles.size(); ++j)
+        {
+            std::cout << scopi::closest_points_dispatcher<dim>::dispatch(*particles[i], *particles[j]) << std::endl;
+        }
+    }
+
+    // std::cout << "particles.pos() = \n" << particles.pos() << "\n\n";
+}
