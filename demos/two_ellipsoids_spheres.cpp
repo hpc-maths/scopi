faf7f26d40d68b471ff55f9775639cac6441efcf
Helene Bloch <helene.bloch@cea.fr>
2021-10-18 10:51:37 +0200

    Add some tests

---
+++ demos/two_ellipsoids_spheres.cpp
@@ -0,0 +1,23 @@
+#include <xtensor/xmath.hpp>
+#include <scopi/objects/types/superellipsoid.hpp>
+#include <scopi/solvers/mosek.hpp>
+
+int main()
+{
+    constexpr std::size_t dim = 2;
+    double PI = xt::numeric_constants<double>::PI;
+    double dt = .005;
+    std::size_t total_it = 1000;
+    scopi::scopi_container<dim> particles;
+
+    scopi::superellipsoid<dim> s1({{-0.2, 0., 0.}}, {scopi::quaternion(PI/4)}, {{.1, .1, .1}}, {{1, 1}});
+    scopi::superellipsoid<dim> s2({{0.2, 0., 0.}}, {scopi::quaternion(-PI/4)}, {{.1, .1, .1}}, {{1, 1}});
+    particles.push_back(s1, {{0, 0, 0}}, {{0.25, 0, 0}}, 0, 0, {{0, 0, 0}});
+    particles.push_back(s2, {{0, 0, 0}}, {{-0.25, 0, 0}}, 0, 0, {{0, 0, 0}});
+
+    std::size_t active_ptr = 0; // without obstacles
+
+    scopi::mosek_solver(particles, dt, total_it, active_ptr);
+
+    return 0;
+}
