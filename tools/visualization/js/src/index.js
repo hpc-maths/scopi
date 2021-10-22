b700d198b488d95a07e699b3139050df583dea5d
loic.gouarin <loic.gouarin@gmail.com>
2021-10-22 06:50:40 +0200

    add new threejs implementation

---
+++ tools/visualization/js/src/index.js
@@ -0,0 +1,201 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
+
+let camera, scene, renderer;
+let container, controls, stats, gui, guiStatsEl;
+var oFiles = [];
+var guiFrame;
+var clock = new THREE.Clock();
+
+var options = {
+    "refresh": 0.2,
+    "pause": false,
+    "current_frame": 0,
+};
+
+document.getElementById("uploadResult").addEventListener("change", function (event) {
+
+    oFiles = Array.from(document.getElementById("uploadResult").files)
+        .sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+
+    guiFrame.max(oFiles.length - 1);
+    options.current_frame = 0;
+    drawObjects();
+    clock.stop();
+    clock.start();
+});
+
+init();
+animate();
+
+const sphereObject = function () {
+    const position = new THREE.Vector3();
+    const rotation = new THREE.Euler();
+    const quaternion = new THREE.Quaternion();
+    const scale = new THREE.Vector3();
+    return function (obj, matrix) {
+        position.x = obj.position[0];
+        position.y = obj.position[1];
+        position.z = 0;
+
+        quaternion.x = obj.quaternion[1];
+        quaternion.y = obj.quaternion[2];
+        quaternion.z = obj.quaternion[3];
+        quaternion.w = obj.quaternion[0];
+        quaternion.normalize();
+
+        scale.x = obj.radius[0];
+        scale.y = obj.radius[1];
+        scale.z = 0.001;
+        matrix.compose(position, quaternion, scale);
+    };
+}();
+
+function drawObjects() {
+
+    if (options.current_frame < oFiles.length) {
+        const reader = new FileReader();
+        reader.addEventListener('load', (event) => {
+            const data = JSON.parse(reader.result);
+            const objects = data.objects;
+            const contacts = data.contacts;
+
+            clean(scene);
+
+            var geometry = new THREE.SphereGeometry(1, 16, 16);
+            const material = new THREE.MeshBasicMaterial({ color: 'red' });
+            var mesh = new THREE.InstancedMesh(geometry, material, objects.length);
+            scene.add(mesh);
+
+
+            const matrix = new THREE.Matrix4();
+
+            objects.forEach((obj, index) => {
+                sphereObject(obj, matrix);
+                mesh.setMatrixAt(index, matrix);
+            });
+
+            const points = [];
+            contacts.forEach((obj, index) => {
+                points.push(new THREE.Vector3(obj.pi[0], obj.pi[1], 0.));
+                points.push(new THREE.Vector3(obj.pj[0], obj.pj[1], 0.));
+            });
+            const line_geometry = new THREE.BufferGeometry().setFromPoints(points);
+            const line_material = new THREE.LineBasicMaterial({
+                color: 'green',
+            });
+            var line_mesh = new THREE.LineSegments(line_geometry, line_material);
+            scene.add(line_mesh);
+
+            guiStatsEl.innerHTML = [
+
+                '<i>Number of objects</i>: ' + objects.length,
+                '<i>Number of contacts</i>: ' + contacts.length
+
+            ].join('<br/>');
+
+        });
+        reader.readAsText(oFiles[options.current_frame]);
+    }
+    else {
+        options.current_frame = 0;
+    }
+};
+
+function clean(obj) {
+    while (obj.children.length > 0) {
+        clean(obj.children[0])
+        obj.remove(obj.children[0]);
+    }
+    if (obj.geometry) obj.geometry.dispose()
+
+    if (obj.material) {
+        //in case of map, bumpMap, normalMap, envMap ...
+        Object.keys(obj.material).forEach(prop => {
+            if (!obj.material[prop])
+                return
+            if (obj.material[prop] !== null && typeof obj.material[prop].dispose === 'function')
+                obj.material[prop].dispose()
+        })
+        obj.material.dispose()
+    }
+}
+
+function init() {
+    clock.stop();
+    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.z = 100;
+
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
+    const container = document.getElementById('container');
+    container.appendChild(renderer.domElement);
+
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    window.addEventListener('resize', onWindowResize);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+
+    gui = new GUI();
+    gui.add(options, 'refresh', 0.1, 5, 0.1);
+    guiFrame = gui.add(options, 'current_frame', 0, 0, 1).listen();
+    guiFrame.onChange(
+        function (value) {
+            drawObjects();
+        });
+
+    gui.add(options, 'pause').name('Pause');
+
+    var obj = {
+        reset: function () {
+            controls.reset();
+        }
+    }
+    gui.add(obj, 'reset').name("Reset camera");
+
+    const infoFolder = gui.addFolder('Informations');
+
+    guiStatsEl = document.createElement('li');
+    guiStatsEl.classList.add('gui-stats');
+
+    infoFolder.__ul.appendChild(guiStatsEl);
+    infoFolder.open();
+}
+
+function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (!options.pause && clock.running && clock.getElapsedTime() > options.refresh) {
+        clock.stop();
+        clock.start();
+        options.current_frame++;
+        drawObjects();
+    }
+
+    stats.update();
+    controls.update();
+    render();
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
