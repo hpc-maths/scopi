1acff2ee21217a203cda3a566eb681e9ddf2d47d
Helene Bloch <helene.bloch@cea.fr>
2021-10-26 15:12:26 +0200

    Clean code

--- demos/two_ellipsoids.cpp
+++ demos/two_ellipsoids.cpp
@@ -18,7 +18,7 @@ int main()
     std::size_t active_ptr = 0; // without obstacles
 
     // scopi::mosek_solver(particles, dt, total_it, active_ptr);
-    scopi::MosekSolver<dim, scopi::useMosekSolver> mosek_solver(particles, dt, active_ptr);
+    scopi::MosekSolver<dim, scopi::useScsSolver> mosek_solver(particles, dt, active_ptr);
     mosek_solver.solve(total_it);
     return 0;
 }
