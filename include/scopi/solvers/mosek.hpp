0c47a1292f6f93f46770072d06f2c05c0da226e3
Helene Bloch <helene.bloch@cea.fr>
2021-11-09 11:56:04 +0100

    Matrix A as member of the classes ScsSolver and MosekSolver

--- include/scopi/solvers/mosek.hpp
+++ include/scopi/solvers/mosek.hpp
@@ -101,7 +101,7 @@ namespace scopi
 
                 // create mass and inertia matrices
                 tic();
-                auto constraintMatrix = _solver.createMatrixConstraint(contacts);
+                _solver.createMatrixConstraint(contacts);
                 _solver.createMatrixMass();
                 _solver.createVectorC();
                 _solver.createVectorDistances(contacts);
@@ -112,7 +112,7 @@ namespace scopi
                 std::cout << "----> Create optimization problem " << nite << std::endl;
                 tic();
                 std::vector<double> sol;
-                auto nbIter = _solver.solveOptimizationProbelm(contacts, constraintMatrix, sol);
+                auto nbIter = _solver.solveOptimizationProbelm(contacts, sol);
                 auto duration5 = toc();
                 std::cout << "----> CPUTIME : solve = " << duration5 << std::endl;
                 std::cout << "iterations : " << nbIter << std::endl;
