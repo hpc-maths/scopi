5fc29d4f289cd8d87a84f65fea79bb644fa17285
Helene Bloch <helene.bloch@cea.fr>
2021-11-10 12:20:21 +0100

    Try to use MKL (does not compile)

--- include/scopi/solvers/UzawaSolver.hpp
+++ include/scopi/solvers/UzawaSolver.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+// #include "mkl_service.h"
+#include "mkl_spblas.h"
+
 namespace scopi{
     template<std::size_t dim>
         class UzawaSolver : public OptimizationSolver<dim>
@@ -34,6 +37,31 @@ namespace scopi{
     template<std::size_t dim>
         int UzawaSolver<dim>::solveOptimizationProbelm(std::vector<scopi::neighbor<dim>>& contacts)
         {
+            /*
+            constexpr MKL_INT M = 4;
+            constexpr MKL_INT N = 6;
+            constexpr MKL_INT NNZ = 8;
+
+            float   csrVal[NNZ]      = { 1., 2., 3., 4., 5., 6., 7., 8. };
+            MKL_INT csrColInd[NNZ]   = { 0, 1, 1, 3, 2, 3, 4, 5 };
+            MKL_INT csrRowPtr[M + 1] = { 0, 2, 4, 7, 8 };
+
+            sparse_matrix_t csrA;
+            mkl_sparse_s_create_csr(&csrA, SPARSE_INDEX_BASE_ZERO, N, M,
+                    csrRowPtr, csrRowPtr + 1, csrColInd, csrVal);
+
+            float x[N]  = { 0., 1., 2., 3., 4., 5. };
+            float y[N]  = { 0., 0., 0., 0., 0., 0. };
+            float alpha = 1., beta = 0.;
+
+            matrix_descr descrA;
+            descrA.type = SPARSE_MATRIX_TYPE_GENERAL;
+
+            mkl_sparse_optimize(csrA);
+            mkl_sparse_s_mv(SPARSE_OPERATION_NON_TRANSPOSE, alpha, csrA, descrA, x, beta, y);
+            mkl_sparse_destroy(csrA);
+            */
+
             return 0;
         }
 
