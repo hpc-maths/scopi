03d20aebd3181b3086a39a86aca71755479a06f2
Sylvain Faure <sylvain.faure@signactif.com>
2021-11-29 16:46:51 +0100

    contacts

---
+++ include/scopi/contact/base.hpp
@@ -0,0 +1,28 @@
+#include "../container.hpp"
+#include "../objects/methods/closest_points.hpp"
+#include "../objects/neighbor.hpp"
+#include <nanoflann.hpp>
+
+namespace scopi
+{
+
+    template <class D>
+    class contact_base: public crtp_base<D>
+    {
+    public:
+        contact_base(double dmax): _dmax(dmax){}
+        template <std::size_t dim>
+        std::vector<scopi::neighbor<dim>> run(scopi_container<dim>& particles, std::size_t active_ptr);
+    protected:
+      double _dmax;
+    };
+
+
+    template <class D>
+    template <std::size_t dim>
+    std::vector<scopi::neighbor<dim>> contact_base<D>::run(scopi_container<dim>& particles, std::size_t active_ptr)
+    {
+        return this->derived_cast().run_impl(particles, active_ptr);
+    }
+
+}
