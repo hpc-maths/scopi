9ce17d2e71858f984b2f184d638e9c3fc399282a
Helene Bloch <helene.bloch@cea.fr>
2021-12-01 15:26:58 +0100

    Start writting vap_pdf::aPrioryvelocity (physics is missing)

--- include/scopi/vap/vap_pfd.hpp
+++ include/scopi/vap/vap_pfd.hpp
@@ -9,22 +9,52 @@ namespace scopi
         public:
             using base_type = vap_base<vap_pfd>;
             template <std::size_t dim>
-                void aPrioriVelocity_impl(const scopi_container<dim>& particles);
+                void aPrioriVelocity_impl(scopi_container<dim>& particles);
 
             template <std::size_t dim>
-                void updateVelocity_impl(const scopi_container<dim>& particles);
+                void updateVelocity_impl(scopi_container<dim>& particles);
+
+            vap_pfd(std::size_t Nactive, std::size_t active_ptr, double dt);
+
+        private:
+            double f_ext();
+            double t_ext();
 
     };
 
+    vap_pfd::vap_pfd(std::size_t Nactive, std::size_t active_ptr, double dt)
+        : base_type(Nactive, active_ptr, dt)
+    {}
+
     template <std::size_t dim>
-        void vap_pfd::aPrioriVelocity_impl(const scopi_container<dim>& particles)
+        void vap_pfd::aPrioriVelocity_impl(scopi_container<dim>& particles)
         {
-            std::ignore = particles;
+            // TODO what if f_ext and t_ext depends on the particule ?
+            particles.vd() = particles.v() + _dt*f_ext(); // TODO mass is missing
+            // TODO should be dt * (R_i * t_i^{ext , n} - omega'_i * (J_i omega'_i)
+            particles.desired_omega() = particles.omega() + _dt*t_ext(); // TODO momentum is missing
         }
 
     template <std::size_t dim>
-        void vap_pfd::updateVelocity_impl(const scopi_container<dim>& particles)
+        void vap_pfd::updateVelocity_impl(scopi_container<dim>& particles)
         {
-            std::ignore = particles;
+            for (std::size_t i=0; i<_Nactive; ++i)
+            {
+                for (std::size_t d=0; d<dim; ++d)
+                {
+                    // particles.v()(i + _active_ptr)(d) = uadapt(i, d);
+                    // particles.omega()(i + _active_ptr)(d) = wadapt(i, d);
+                }
+            }
         }
+
+    double vap_pfd::f_ext()
+    {
+        return 0.;
+    }
+
+    double vap_pfd::t_ext()
+    {
+        return 0.;
+    }
 }
