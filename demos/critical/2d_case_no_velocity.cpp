868e49c31d43f63179fdf84f98bc5fa197ec276d
Helene Bloch <helene.bloch@cea.fr>
2021-12-02 16:17:39 +0100

    Use spheres instead of ellipsoids for test with gravity

--- demos/critical/2d_case_no_velocity.cpp
+++ demos/critical/2d_case_no_velocity.cpp
@@ -1,5 +1,5 @@
 #include <xtensor/xmath.hpp>
-#include <scopi/objects/types/superellipsoid.hpp>
+#include <scopi/objects/types/sphere.hpp>
 #include <scopi/solvers/mosek.hpp>
 #include <scopi/container.hpp>
 #include <random>
@@ -10,7 +10,6 @@ int main()
 {
 
     constexpr std::size_t dim = 2;
-    double PI = xt::numeric_constants<double>::PI;
     double dt = .01;
     std::size_t total_it = 100;
     scopi::scopi_container<dim> particles;
@@ -19,29 +18,23 @@ int main()
 
     std::default_random_engine generator;
     std::uniform_real_distribution<double> distrib_r(0.2, 0.4);
-    std::uniform_real_distribution<double> distrib_r2(0.2, 0.4);
     std::uniform_real_distribution<double> distrib_move_x(-0.1, 0.1);
     std::uniform_real_distribution<double> distrib_move_y(-0.1, 0.1);
-    std::uniform_real_distribution<double> distrib_rot(0, PI);
 
     for(int i = 0; i < n; ++i)
     {
         for(int j = 0; j < n; ++j)
         {
-            double rot = distrib_rot(generator);
             double r = distrib_r(generator);
-            double r2 = distrib_r2(generator);
             double x = -n + (i + 0.5) + distrib_move_x(generator);
             double y = -n/2. + (j + 0.5) + distrib_move_y(generator);
-            scopi::superellipsoid<dim> s1({ {x, y}}, {scopi::quaternion(rot)}, {{r, r2}}, {{1}});
+            scopi::sphere<dim> s1({ {x, y}}, r);
             particles.push_back(s1, {{0, 0}}, {{0., 0.}}, 0, 0, {{0, 0}});
 
-            rot = distrib_rot(generator);
             r = distrib_r(generator);
-            r2 = distrib_r2(generator);
             x = (i + 0.5) + distrib_move_x(generator);
             y = -n/2. + (j + 0.5) + distrib_move_y(generator);
-            scopi::superellipsoid<dim> s2({ {x, y}}, {scopi::quaternion(rot)}, {{r, r2}}, {{1}});
+            scopi::sphere<dim> s2({ {x, y}}, r);
             particles.push_back(s2, {{0, 0}}, {{0., 0.}}, 0, 0, {{0, 0}});
         }
     }
