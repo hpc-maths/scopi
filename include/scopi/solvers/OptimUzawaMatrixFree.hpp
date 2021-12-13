68eed0e00cf835310bc63dd4394dc496fc9a4d2c
Helene Bloch <helene.bloch@cea.fr>
2021-12-13 15:21:56 +0100

    OptimUzawaMAtrixFree: remove MKL related fonctions

--- include/scopi/solvers/OptimUzawaMatrixFree.hpp
+++ include/scopi/solvers/OptimUzawaMatrixFree.hpp
@@ -26,7 +26,6 @@ namespace scopi{
             int getNbActiveContacts_impl();
 
         private:
-            void printCrsMatrix(const sparse_matrix_t);
             void gemv_invP();
             void gemv_A(const std::vector<scopi::neighbor<dim>>& contacts);
             void gemv_transposeA(const std::vector<scopi::neighbor<dim>>& contacts);
@@ -39,15 +38,6 @@ namespace scopi{
             xt::xtensor<double, 1> _L;
             xt::xtensor<double, 1> _R;
             int _nbActiveContacts = 0;
-            sparse_matrix_t _A;
-            struct matrix_descr _descrA;
-            std::vector<MKL_INT> _A_coo_row;
-            std::vector<MKL_INT> _A_coo_col;
-            std::vector<double> _A_coo_val;
-            sparse_matrix_t _invP;
-            struct matrix_descr _descrInvP;
-            sparse_status_t _status;
-
     };
 
     template<std::size_t dim>
@@ -61,112 +51,12 @@ namespace scopi{
     template<std::size_t dim>
         void OptimUzawaMatrixFree<dim>::createMatrixConstraint_impl(const std::vector<scopi::neighbor<dim>>& contacts)
         {
-            this->createMatrixConstraintCoo(contacts, _A_coo_row, _A_coo_col, _A_coo_val, 0);
-
-            sparse_matrix_t A_coo;
-            _status =  mkl_sparse_d_create_coo
-                (&A_coo,
-                 SPARSE_INDEX_BASE_ZERO,
-                 contacts.size(),    // number of rows
-                 6*this->_Nactive,   // number of cols
-                 _A_coo_val.size(),   // number of non-zero elements
-                 _A_coo_row.data(),
-                 _A_coo_col.data(),
-                 _A_coo_val.data()
-                );
-            if (_status != SPARSE_STATUS_SUCCESS)
-            {
-                std::cout << " Error in mkl_sparse_d_create_coo for matrix A: " << _status << std::endl;
-            }
-
-            _status = mkl_sparse_convert_csr
-                (A_coo,
-                 SPARSE_OPERATION_NON_TRANSPOSE,
-                 &_A
-                );
-            if (_status != SPARSE_STATUS_SUCCESS)
-            {
-                std::cout << " Error in mkl_sparse_convert_csr for matrix A: " << _status << std::endl;
-            }
-
-            _descrA.type = SPARSE_MATRIX_TYPE_GENERAL;
-
-            _status = mkl_sparse_set_mv_hint(_A, SPARSE_OPERATION_NON_TRANSPOSE, _descrA, 1 );
-            if (_status != SPARSE_STATUS_SUCCESS && _status != SPARSE_STATUS_NOT_SUPPORTED)
-            {
-                std::cout << " Error in mkl_sparse_set_mv_hint for matrix A SPARSE_OPERATION_NON_TRANSPOSE: " << _status << std::endl;
-            }
-
-            _status = mkl_sparse_set_mv_hint(_A, SPARSE_OPERATION_TRANSPOSE, _descrA, 1 );
-            if (_status != SPARSE_STATUS_SUCCESS && _status != SPARSE_STATUS_NOT_SUPPORTED)
-            {
-                std::cout << " Error in mkl_sparse_set_mv_hint for matrix A SPARSE_OPERATION_TRANSPOSE: " << _status << std::endl;
-            }
-
-            _status = mkl_sparse_optimize ( _A );
-            if (_status != SPARSE_STATUS_SUCCESS)
-            {
-                std::cout << " Error in mkl_sparse_optimize for matrix A: " << _status << std::endl;
-            }
+            std::ignore = contacts;
         }
 
     template<std::size_t dim>
         void OptimUzawaMatrixFree<dim>::createMatrixMass_impl()
         {
-            std::vector<MKL_INT> invP_csr_row;
-            std::vector<MKL_INT> invP_csr_col;
-            std::vector<double> invP_csr_val;
-            invP_csr_col.reserve(6*this->_Nactive);
-            invP_csr_row.reserve(6*this->_Nactive+1);
-            invP_csr_val.reserve(6*this->_Nactive);
-
-            for (std::size_t i=0; i<this->_Nactive; ++i)
-            {
-                for (std::size_t d=0; d<3; ++d)
-                {
-                    invP_csr_row.push_back(3*i + d);
-                    invP_csr_col.push_back(3*i + d);
-                    invP_csr_val.push_back(1./this->_mass); // TODO: add mass into particles
-                }
-            }
-            for (std::size_t i=0; i<this->_Nactive; ++i)
-            {
-                for (std::size_t d=0; d<3; ++d)
-                {
-                    invP_csr_row.push_back(3*this->_Nactive + 3*i + d);
-                    invP_csr_col.push_back(3*this->_Nactive + 3*i + d);
-                    invP_csr_val.push_back(1./this->_moment);
-                }
-            }
-            invP_csr_row.push_back(6*this->_Nactive);
-
-            _status = mkl_sparse_d_create_csr( &_invP,
-                    SPARSE_INDEX_BASE_ZERO,
-                    6*this->_Nactive,    // number of rows
-                    6*this->_Nactive,    // number of cols
-                    invP_csr_row.data(),
-                    invP_csr_row.data()+1,
-                    invP_csr_col.data(),
-                    invP_csr_val.data() );
-            if (_status != SPARSE_STATUS_SUCCESS)
-            {
-                std::cout << " Error in mkl_sparse_d_create_csr for matrix invP: " << _status << std::endl;
-            }
-
-            _descrInvP.type = SPARSE_MATRIX_TYPE_DIAGONAL;
-            _descrInvP.diag = SPARSE_DIAG_NON_UNIT;
-
-            _status = mkl_sparse_set_mv_hint(_invP, SPARSE_OPERATION_NON_TRANSPOSE, _descrInvP, 1 );
-            if (_status != SPARSE_STATUS_SUCCESS && _status != SPARSE_STATUS_NOT_SUPPORTED)
-            {
-                std::cout << " Error in mkl_sparse_set_mv_hint for matrix invP: " << _status << std::endl;
-            }
-
-            _status = mkl_sparse_optimize ( _invP );
-            if (_status != SPARSE_STATUS_SUCCESS)
-            {
-                std::cout << " Error in mkl_sparse_optimize for matrix invP: " << _status << std::endl;
-            }
         }
 
     template<std::size_t dim>
@@ -221,60 +111,12 @@ namespace scopi{
     template<std::size_t dim>
         void OptimUzawaMatrixFree<dim>::freeMemory_impl()
         {
-            mkl_sparse_destroy ( _invP );
-            mkl_sparse_destroy ( _A );
-            _A_coo_row.clear();
-            _A_coo_col.clear();
-            _A_coo_val.clear();
         }
 
     template<std::size_t dim>
         int OptimUzawaMatrixFree<dim>::getNbActiveContacts_impl()
         {
             return xt::sum(xt::where(_L > 0., xt::ones_like(_L), xt::zeros_like(_L)))();
-        }
-
-    template<std::size_t dim>
-        void OptimUzawaMatrixFree<dim>::printCrsMatrix(const sparse_matrix_t A)
-        {
-            MKL_INT* csr_row_begin_ptr = NULL;
-            MKL_INT* csr_row_end_ptr = NULL;
-            MKL_INT* csr_col_ptr = NULL;
-            double* csr_val_ptr = NULL;
-            sparse_index_base_t indexing;
-            MKL_INT nbRows;
-            MKL_INT nbCols;
-            _status = mkl_sparse_d_export_csr
-                (
-                 A,
-                 &indexing,
-                 &nbRows,
-                 &nbCols,
-                 &csr_row_begin_ptr,
-                 &csr_row_end_ptr,
-                 &csr_col_ptr,
-                 &csr_val_ptr
-                );
-
-            if (_status != SPARSE_STATUS_SUCCESS)
-            {
-                std::cout << " Error in mkl_sparse_d_export_csr: " << _status << std::endl;
-            }
-
-            std::cout << "\nMatrix with " << nbRows << " rows and " << nbCols << " columns\n";
-            std::cout << "RESULTANT MATRIX:\nrow# : (column, value) (column, value)\n";
-            int ii = 0;
-            for( int i = 0; i < nbRows; i++ )
-            {
-                std::cout << "row#" << i << ": ";
-                for(MKL_INT j = csr_row_begin_ptr[i]; j < csr_row_end_ptr[i]; j++ )
-                {
-                    std::cout << " (" << csr_col_ptr[ii] << ", " << csr_val_ptr[ii] << ")";
-                    ii++;
-                }
-                std::cout << std::endl;
-            }
-            std::cout << "_____________________________________________________________________  \n" ;
         }
 
     template<std::size_t dim>
