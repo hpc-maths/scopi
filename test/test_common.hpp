8fc86e68b3a42b4cd80cde7fadfc79651d545fb0
Helene <helenebloch@tutanota.com>
2022-03-30 18:25:01 +0200

    test_friction with doctest

--- test/test_common.hpp
+++ test/test_common.hpp
@@ -27,5 +27,13 @@ namespace scopi
 
     #define SOLVER_WITH_CONTACT_FRICTION(dim, contact, vap) \
         ScopiSolver<dim, OptimMosek<MatrixOptimSolverFriction>, contact, vap> // friction
+                                                                              
+    #define DOCTEST_VALUE_PARAMETERIZED_DATA(data, data_container) \
+        static size_t _doctest_subcase_idx = 0; \
+        std::for_each(data_container.begin(), data_container.end(), [&](const auto& in) {  \
+            DOCTEST_SUBCASE((std::string(#data_container "[") + \
+                            std::to_string(_doctest_subcase_idx++) + "]").c_str()) { data = in; } \
+        }); \
+        _doctest_subcase_idx = 0;
 
 }
