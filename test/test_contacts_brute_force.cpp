5739c8602283ac08e57498ea1b45a2cbc1fdbfe6
Helene <helenebloch@tutanota.com>
2022-03-30 11:34:32 +0200

    test_contacts with doctest

--- test/test_contacts_brute_force.cpp
+++ test/test_contacts_brute_force.cpp
@@ -1,4 +1,4 @@
-#include <gtest/gtest.h>
+#include "doctest/doctest.h"
 #include "utils.hpp"
 
 #include <scopi/objects/types/sphere.hpp>
@@ -8,56 +8,51 @@
 
 namespace scopi
 {
-
-    class ContactsBruteForceTest  : public ::testing::Test {
-        protected:
-            ContactsBruteForceTest()
-            {
-                constexpr std::size_t dim = 2;
-                scopi_container<dim> particles;
-
-                sphere<dim> s1({{0., 0.}}, 0.1);
-                sphere<dim> s2({{1., 1.}}, 0.2);
-                sphere<dim> s3({{5., 10.}}, 0.1);
-
-                particles.push_back(s1);
-                particles.push_back(s2);
-                particles.push_back(s3);
-
-                contact_brute_force cont(2);
-                m_contacts = cont.run(particles, 0);
-            }
-            std::vector<neighbor<2>> m_contacts;
-    };
-
-    TEST_F(ContactsBruteForceTest, nbContacts)
+    TEST_CASE("Contacts brute force")
     {
-        EXPECT_EQ(m_contacts.size(), 1);
-    }
+        constexpr std::size_t dim = 2;
+        scopi_container<dim> particles;
 
-    TEST_F(ContactsBruteForceTest, particlesInContact)
-    {
-        EXPECT_EQ(m_contacts[0].i, 0);
-        EXPECT_EQ(m_contacts[0].j, 1);
-    }
+        sphere<dim> s1({{0., 0.}}, 0.1);
+        sphere<dim> s2({{1., 1.}}, 0.2);
+        sphere<dim> s3({{5., 10.}}, 0.1);
 
-    TEST_F(ContactsBruteForceTest, distance)
-    {
-        EXPECT_DOUBLE_EQ(m_contacts[0].dij, std::sqrt(2.)-0.1-0.2);
-    }
+        particles.push_back(s1);
+        particles.push_back(s2);
+        particles.push_back(s3);
 
-    TEST_F(ContactsBruteForceTest, normal)
-    {
-        EXPECT_DOUBLE_EQ(m_contacts[0].nij(0), -1./std::sqrt(2.));
-        EXPECT_DOUBLE_EQ(m_contacts[0].nij(1), -1./std::sqrt(2.));
-    }
+        contact_brute_force cont(2);
+        auto contacts = cont.run(particles, 0);
 
-    TEST_F(ContactsBruteForceTest, position)
-    {
-        EXPECT_DOUBLE_EQ(m_contacts[0].pi(0), 0.1*std::sqrt(2.)/2.);
-        EXPECT_DOUBLE_EQ(m_contacts[0].pi(1), 0.1*std::sqrt(2.)/2.);
-        EXPECT_DOUBLE_EQ(m_contacts[0].pj(0), 1.-0.2*std::sqrt(2.)/2.);
-        EXPECT_DOUBLE_EQ(m_contacts[0].pj(1), 1.-0.2*std::sqrt(2.)/2.);
+        SUBCASE("nbContacts")
+        {
+            CHECK(contacts.size() == 1);
+        }
+
+        SUBCASE("particles in contacts")
+        {
+            CHECK(contacts[0].i == 0);
+            CHECK(contacts[0].j == 1);
+        }
+
+        SUBCASE("distance")
+        {
+            REQUIRE(contacts[0].dij == doctest::Approx( std::sqrt(2.)-0.1-0.2));
+        }
+
+        SUBCASE("normal")
+        {
+            REQUIRE(contacts[0].nij(0) == doctest::Approx( -1./std::sqrt(2.)));
+            REQUIRE(contacts[0].nij(1) == doctest::Approx( -1./std::sqrt(2.)));
+        }
+
+        SUBCASE("position")
+        {
+            REQUIRE(contacts[0].pi(0) == doctest::Approx( 0.1*std::sqrt(2.)/2.));
+            REQUIRE(contacts[0].pi(1) == doctest::Approx( 0.1*std::sqrt(2.)/2.));
+            REQUIRE(contacts[0].pj(0) == doctest::Approx( 1.-0.2*std::sqrt(2.)/2.));
+            REQUIRE(contacts[0].pj(1) == doctest::Approx( 1.-0.2*std::sqrt(2.)/2.));
+        }
     }
 
 }
