520aefee2173a40fa8d4d19100e9acac9fe5ff8e
Helene <helenebloch@tutanota.com>
2022-03-31 17:45:59 +0200

    test_friction: simplify parameters

--- test/test_friction.cpp
+++ test/test_friction.cpp
@@ -78,29 +78,29 @@ namespace scopi {
         CHECK(diffFile("./Results/scopi_objects_0099.json", "../test/references/2d_case_spheres_friction.json", tolerance));
     }
 
-    TEST_CASE_TEMPLATE("sphere inclined plan", SolverType, SOLVER_WITH_CONTACT_FRICTION(2, contact_kdtree, vap_fpd), SOLVER_WITH_CONTACT_FRICTION(2, contact_brute_force, vap_fpd))
+    TEST_CASE_TEMPLATE("sphere inclined plan friction", SolverType, SOLVER_WITH_CONTACT_FRICTION(2, contact_kdtree, vap_fpd), SOLVER_WITH_CONTACT_FRICTION(2, contact_brute_force, vap_fpd))
     {
-        std::tuple<double, std::size_t, double, double, double, double> data;
-        std::vector<std::tuple<double, std::size_t, double, double, double, double>>
-            data_container({std::make_tuple(0.005, 2000, 0.1, PI/6., 0.000508866, 0.000485907),
-                            std::make_tuple(0.005, 2000, 0.1, PI/4., 0.000513437, 0.000464869),
-                            std::make_tuple(0.005, 2000, 0.1, PI/3., 0.000516023, 0.000428305),
-                            std::make_tuple(0.005, 2000, 0.5, PI/6., 0.00049975, 0.000499785),
-                            std::make_tuple(0.005, 2000, 0.5, PI/4., 0.000499728, 0.000500104),
-                            std::make_tuple(0.005, 2000, 0.5, PI/3., 0.000554791, 0.00044186), 
-                            std::make_tuple(0.005, 2000, 1., PI/6., 0.000499751, 0.000499752), 
-                            std::make_tuple(0.005, 2000, 1., PI/4., 0.000499751, 0.00049976),
-                            std::make_tuple(0.005, 2000, 1., PI/3., 0.000499735, 0.000499765)});
+        std::tuple<double, double, double, double> data;
+        std::vector<std::tuple<double, double, double, double>>
+            data_container({std::make_tuple(0.1, PI/6., 0.000508866, 0.000485907),
+                            std::make_tuple(0.1, PI/4., 0.000513437, 0.000464869),
+                            std::make_tuple(0.1, PI/3., 0.000516023, 0.000428305),
+                            std::make_tuple(0.5, PI/6., 0.00049975, 0.000499785),
+                            std::make_tuple(0.5, PI/4., 0.000499728, 0.000500104),
+                            std::make_tuple(0.5, PI/3., 0.000554791, 0.00044186), 
+                            std::make_tuple(1., PI/6., 0.000499751, 0.000499752), 
+                            std::make_tuple(1., PI/4., 0.000499751, 0.00049976),
+                            std::make_tuple(1., PI/3., 0.000499735, 0.000499765)});
 
         DOCTEST_VALUE_PARAMETERIZED_DATA(data, data_container);
 
         static constexpr std::size_t dim = 2;
         double radius = 1.;
         double g = 1.;
-        double dt = std::get<0>(data);
-        std::size_t total_it = std::get<1>(data);
-        double mu = std::get<2>(data);
-        double alpha = std::get<3>(data);
+        double dt = 0.005;
+        std::size_t total_it = 2000;
+        double mu = std::get<0>(data);
+        double alpha = std::get<1>(data);
 
         auto prop = property<dim>().mass(1.).moment_inertia(1.*radius*radius/2.);
         plan<dim> p({{-radius*std::cos(PI/2.-alpha), -radius*std::sin(PI/2.-alpha)}}, PI/2.-alpha);
@@ -122,8 +122,8 @@ namespace scopi {
         double err_pos = xt::linalg::norm(pos(1) - pos_analytical) / xt::linalg::norm(pos_analytical);
         double err_omega = std::abs((omega(1)-omega_analytical)/omega_analytical);
 
-        REQUIRE(err_pos == doctest::Approx(std::get<4>(data)));
-        REQUIRE(err_omega == doctest::Approx(std::get<5>(data)));
+        REQUIRE(err_pos == doctest::Approx(std::get<2>(data)));
+        REQUIRE(err_omega == doctest::Approx(std::get<3>(data)));
     }
 
 }
