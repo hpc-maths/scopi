8e9f901e1dae549a306d3f683b2522d751eb675c
Helene <helenebloch@tutanota.com>
2022-04-06 17:22:25 +0200

    MatrixOptimSolver: change gamma and contacts

--- include/scopi/solvers/MatrixOptimSolverViscosity.hpp
+++ include/scopi/solvers/MatrixOptimSolverViscosity.hpp
@@ -21,8 +21,8 @@ namespace scopi
         void create_matrix_constraint_coo(const scopi_container<dim>& particles,
                                           const std::vector<neighbor<dim>>& contacts,
                                           std::size_t firstCol);
+        void set_gamma(const std::vector<neighbor<dim>>& contacts_new);
         void update_gamma(const std::vector<neighbor<dim>>& contacts);
-        void set_gamma(const std::vector<neighbor<dim>>& contacts);
 
         std::size_t m_nparticles;
         double m_dt;
@@ -119,11 +119,48 @@ namespace scopi
     {}
 
     template<std::size_t dim>
-    void MatrixOptimSolverViscosity<dim>::set_gamma(const std::vector<neighbor<dim>>& contacts)
-    {}
+    void MatrixOptimSolverViscosity<dim>::set_gamma(const std::vector<neighbor<dim>>& contacts_new)
+    {
+        m_gamma.resize(contacts_new.size());
+        if(m_contacts_old.size() > 0)
+        {
+            for (std::size_t index_new = 0; index_new < contacts_new.size(); ++index_new)
+            {
+                auto cn = contacts_new[index_new];
+                std::size_t index_old = 0.;
+                auto co = m_contacts_old[index_old];
+                while (co.i < cn.i && index_old < m_contacts_old.size())
+                {
+                    index_old ++;
+                    co = m_contacts_old[index_old];
+                }
+                while (co.j < cn.j && co.i == cn.i)
+                {
+                    index_old ++;
+                    co = m_contacts_old[index_old];
+                }
+                if(co.i == cn.i && co.j == cn.j)
+                    m_gamma[index_new] = m_gamma_old[index_old];
+                else
+                    m_gamma[index_new] = 0.;
+            }
+        }
+        else
+        {
+            for(auto& g : m_gamma)
+                g = 0.;
+        }
+    }
 
     template<std::size_t dim>
     void MatrixOptimSolverViscosity<dim>::update_gamma(const std::vector<neighbor<dim>>& contacts)
-    {}
+    {
+        m_contacts_old = contacts;
+        m_gamma_old.resize(m_gamma.size());
+        for (std::size_t i = 0; i < m_gamma_old.size(); ++i)
+        {
+            m_gamma_old[i] = m_gamma[i]; // TODO m_gamma_old[i] = m_gamma[i] - m_dt * lambda[i]
+        }
+    }
 }
 
