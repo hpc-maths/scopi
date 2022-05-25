8686efa6e0573e44b46741a0bff6814f1582a3b4
Helene <helenebloch@tutanota.com>
2022-05-25 14:09:15 +0200

    add test for viscosity with friction test with vertical gravity

--- test/test_viscosity.cpp
+++ test/test_viscosity.cpp
@@ -40,6 +40,34 @@ namespace scopi {
         CHECK(diffFile("./Results/scopi_objects_0199.json", "../test/references/sphere_plan_viscosity.json", tolerance));
     }
 
+    TEST_CASE_TEMPLATE("sphere plan viscosity friction vertical", SolverType, SOLVER_VISCOUS_WITH_FRICTION(2, contact_kdtree, vap_fpd), SOLVER_VISCOUS_WITH_FRICTION(2, contact_brute_force, vap_fpd))
+    {
+        constexpr std::size_t dim = 2;
+        double PI = xt::numeric_constants<double>::PI;
+
+        double radius = 1.;
+        double g = radius;
+        double h = 1.5*radius;
+        auto prop = scopi::property<dim>().mass(1.).moment_inertia(1.*radius*radius/2.);
+
+        double dt = 0.05;
+        std::size_t total_it = 100;
+
+        scopi::scopi_container<dim> particles;
+        scopi::plan<dim> p({{0., 0.}}, PI/2.);
+        scopi::sphere<dim> s({{0., h}}, radius);
+        particles.push_back(p, scopi::property<dim>().deactivate());
+        particles.push_back(s, prop.force({{0, -g}}));
+
+        SolverType solver(particles, dt);
+        solver.set_coeff_friction(0.1);
+        solver.solve(total_it);
+        particles.f()(1)(1) *= -1.;
+        solver.solve(2*total_it, total_it);
+
+        CHECK(diffFile("./Results/scopi_objects_0199.json", "../test/references/sphere_plan_viscosity_friction_vertical.json", tolerance));
+    }
+
     TEST_CASE_TEMPLATE("sphere plan viscosity friction", SolverType, SOLVER_VISCOUS_WITH_FRICTION(2, contact_kdtree, vap_fpd), SOLVER_VISCOUS_WITH_FRICTION(2, contact_brute_force, vap_fpd))
     {
         constexpr std::size_t dim = 2;
