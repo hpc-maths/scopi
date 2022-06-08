e192fee620aec65735f9b370d4a97ee8e230fd14
Helene <helenebloch@tutanota.com>
2022-06-08 15:44:44 +0200

    types std::array instead of xt::xtensor_fixed

--- include/scopi/types.hpp
+++ include/scopi/types.hpp
@@ -9,7 +9,7 @@ namespace scopi
     namespace type
     {
         template<std::size_t dim>
-        using position_t = xt::xtensor_fixed<double, xt::xshape<dim>>;
+        using position_t = std::array<double, dim>;
 
         template<std::size_t dim>
         using velocity_t = position_t<dim>;
@@ -18,14 +18,14 @@ namespace scopi
         using force_t = position_t<dim>;
 
         template<std::size_t dim>
-        using moment_t = typename std::conditional<dim == 2, double, xt::xtensor_fixed<double, xt::xshape<dim>>>::type;
+        using moment_t = typename std::conditional<dim == 2, double, position_t<dim>>;
 
         template<std::size_t dim>
-        using matrix_rotation_t = xt::xtensor_fixed<double, xt::xshape<dim, dim>>;
+        using matrix_rotation_t = std::array<std::array<double, dim>, dim>;
 
         template<std::size_t dim>
-        using rotation_t = typename std::conditional<dim == 2, double, xt::xtensor_fixed<double, xt::xshape<dim>>>::type;
+        using rotation_t = typename std::conditional<dim == 2, double, position_t<dim>>;
 
-        using quaternion_t = xt::xtensor_fixed<double, xt::xshape<4>>;
+        using quaternion_t = std::array<double, 4>;
     }
 }
