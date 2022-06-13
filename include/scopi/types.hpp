b690e6b458cdc0f83a256f7588e073b03bf7ac5f
Helene <helenebloch@tutanota.com>
2022-06-13 11:12:16 +0200

    fix types

--- include/scopi/types.hpp
+++ include/scopi/types.hpp
@@ -9,7 +9,7 @@ namespace scopi
     namespace type
     {
         template<std::size_t dim>
-        using position_t = std::array<double, dim>;
+        using position_t = xt::xtensor_fixed<double, xt::xshape<dim>>;
 
         template<std::size_t dim>
         using velocity_t = position_t<dim>;
@@ -18,15 +18,14 @@ namespace scopi
         using force_t = position_t<dim>;
 
         template<std::size_t dim>
-        using moment_t = typename std::conditional<dim == 2, double, position_t<dim>>::type;
+        using moment_t = typename std::conditional<dim == 2, double, xt::xtensor_fixed<double, xt::xshape<dim>>>::type;
 
         template<std::size_t dim>
         using matrix_rotation_t = xt::xtensor_fixed<double, xt::xshape<dim, dim>>;
-        // using matrix_rotation_t = std::array<std::array<double, dim>, dim>;
 
         template<std::size_t dim>
-        using rotation_t = typename std::conditional<dim == 2, double, position_t<dim>>::type;
+        using rotation_t = typename std::conditional<dim == 2, double, xt::xtensor_fixed<double, xt::xshape<dim>>>::type;
 
-        using quaternion_t = std::array<double, 4>;
+        using quaternion_t = xt::xtensor_fixed<double, xt::xshape<4>>;
     }
 }
--- test/test_quaternions.cpp
+++ test/test_quaternions.cpp
@@ -10,19 +10,19 @@ namespace scopi
     TEST_CASE("quaternion")
     {
         auto q = quaternion(PI/3.);
-        REQUIRE(q[0] == doctest::Approx(std::sqrt(3.)/2.));
-        REQUIRE(q[1] == 0.);
-        REQUIRE(q[2] == 0.);
-        REQUIRE(q[3] == doctest::Approx(1./2.));
+        REQUIRE(q(0) == doctest::Approx(std::sqrt(3.)/2.));
+        REQUIRE(q(1) == 0.);
+        REQUIRE(q(2) == 0.);
+        REQUIRE(q(3) == doctest::Approx(1./2.));
     }
     TEST_CASE("normalize normalized quaternion")
     {
         auto q = quaternion(PI/3.);
         normalize(q);
-        REQUIRE(q[0] == doctest::Approx(std::sqrt(3.)/2.));
-        REQUIRE(q[1] == 0.);
-        REQUIRE(q[2] == 0.);
-        REQUIRE(q[3] == doctest::Approx(1./2.));
+        REQUIRE(q(0) == doctest::Approx(std::sqrt(3.)/2.));
+        REQUIRE(q(1) == 0.);
+        REQUIRE(q(2) == 0.);
+        REQUIRE(q(3) == doctest::Approx(1./2.));
     }
 
     TEST_CASE("normalize not-normalized quaternion")
@@ -31,10 +31,10 @@ namespace scopi
         for (auto& elt : q)
             elt *= 2.;
         normalize(q);
-        REQUIRE(q[0] == doctest::Approx(std::sqrt(3.)/2.));
-        REQUIRE(q[1] == 0.);
-        REQUIRE(q[2] == 0.);
-        REQUIRE(q[3] == doctest::Approx(1./2.));
+        REQUIRE(q(0) == doctest::Approx(std::sqrt(3.)/2.));
+        REQUIRE(q(1) == 0.);
+        REQUIRE(q(2) == 0.);
+        REQUIRE(q(3) == doctest::Approx(1./2.));
     }
 
     /*
@@ -42,10 +42,10 @@ namespace scopi
     {
         auto q = quaternion(PI/3.);
         type::quaternion_t q_conj = conj_scopi(q);
-        REQUIRE(q_conj[0] == doctest::Approx(-std::sqrt(3.)/2.));
-        REQUIRE(q_conj[1] == 0.);
-        REQUIRE(q_conj[2] == 0.);
-        REQUIRE(q_conj[3] == doctest::Approx(-1./2.));
+        REQUIRE(q_conj(0) == doctest::Approx(-std::sqrt(3.)/2.));
+        REQUIRE(q_conj(1) == 0.);
+        REQUIRE(q_conj(2) == 0.);
+        REQUIRE(q_conj(3) == doctest::Approx(-1./2.));
     }
     */
 
@@ -55,10 +55,10 @@ namespace scopi
         auto q2 = quaternion(PI/6);
         auto res = mult_quaternion(q1, q2);
 
-        REQUIRE(res[0] == doctest::Approx(std::sqrt(3.)/2.*(std::sqrt(6)+std::sqrt(2))/4 - (std::sqrt(6)-std::sqrt(2))/8));
-        REQUIRE(res[1] == doctest::Approx(0.));
-        REQUIRE(res[2] == doctest::Approx(0.)); 
-        REQUIRE(res[3] == doctest::Approx(std::sqrt(3.)/2.*(std::sqrt(6.)-std::sqrt(2.))/4. + (std::sqrt(6.)+std::sqrt(2.))/8.));
+        REQUIRE(res(0) == doctest::Approx(std::sqrt(3.)/2.*(std::sqrt(6)+std::sqrt(2))/4 - (std::sqrt(6)-std::sqrt(2))/8));
+        REQUIRE(res(1) == doctest::Approx(0.));
+        REQUIRE(res(2) == doctest::Approx(0.)); 
+        REQUIRE(res(3) == doctest::Approx(std::sqrt(3.)/2.*(std::sqrt(6.)-std::sqrt(2.))/4. + (std::sqrt(6.)+std::sqrt(2.))/8.));
     }
 
     TEST_CASE("rotation_matrix")
@@ -67,23 +67,23 @@ namespace scopi
         SUBCASE("2D")
         {
             auto m = rotation_matrix<2>(q);
-            REQUIRE(m[0][0] == doctest::Approx(1./2.));
-            REQUIRE(m[0][1] == doctest::Approx(-std::sqrt(3.)/2.));
-            REQUIRE(m[1][0] == doctest::Approx(std::sqrt(3.)/2.)); 
-            REQUIRE(m[1][1] == doctest::Approx(1./2.)); 
+            REQUIRE(m(0, 0) == doctest::Approx(1./2.));
+            REQUIRE(m(0, 1) == doctest::Approx(-std::sqrt(3.)/2.));
+            REQUIRE(m(1, 0) == doctest::Approx(std::sqrt(3.)/2.)); 
+            REQUIRE(m(1, 1) == doctest::Approx(1./2.)); 
         }
         SUBCASE("3D")
         {
             auto m = rotation_matrix<3>(q);
-            REQUIRE(m[0][0] == doctest::Approx(1./2.));
-            REQUIRE(m[0][1] == doctest::Approx(-std::sqrt(3.)/2.));
-            REQUIRE(m[0][2] == doctest::Approx(0.));
-            REQUIRE(m[1][0] == doctest::Approx(std::sqrt(3.)/2.)); 
-            REQUIRE(m[1][1] == doctest::Approx(1./2.)); 
-            REQUIRE(m[1][2] == doctest::Approx(0.));
-            REQUIRE(m[2][0] == doctest::Approx(0.)); 
-            REQUIRE(m[2][1] == doctest::Approx(0.)); 
-            REQUIRE(m[2][2] == doctest::Approx(1.));
+            REQUIRE(m(0, 0) == doctest::Approx(1./2.));
+            REQUIRE(m(0, 1) == doctest::Approx(-std::sqrt(3.)/2.));
+            REQUIRE(m(0, 2) == doctest::Approx(0.));
+            REQUIRE(m(1, 0) == doctest::Approx(std::sqrt(3.)/2.)); 
+            REQUIRE(m(1, 1) == doctest::Approx(1./2.)); 
+            REQUIRE(m(1, 2) == doctest::Approx(0.));
+            REQUIRE(m(2, 0) == doctest::Approx(0.)); 
+            REQUIRE(m(2, 1) == doctest::Approx(0.)); 
+            REQUIRE(m(2, 2) == doctest::Approx(1.));
         }
     }
 }
