1a02f5ebde773de8a1a97d3b4b99e2d3e3e1182a
Helene <helenebloch@tutanota.com>
2022-06-23 15:19:18 +0200

    fix nesterov

--- include/scopi/solvers/gradient/nesterov.hpp
+++ include/scopi/solvers/gradient/nesterov.hpp
@@ -12,11 +12,11 @@
 
 namespace scopi{
     template<class projection_t = projection_max>
-    class nesterov: public projection_t
+    class projected_gradient: public projection_t
     {
     protected:
-        nesterov(std::size_t max_iter, double rho, double tol_dg, double tol_l);
-        int projection(const sparse_matrix_t& A, const struct matrix_descr& descr, const xt::xtensor<double, 1>& c, xt::xtensor<double, 1>& l);
+        projected_gradient(std::size_t max_iter, double rho, double tol_dg, double tol_l);
+        std::size_t projection(const sparse_matrix_t& A, const struct matrix_descr& descr, const xt::xtensor<double, 1>& c, xt::xtensor<double, 1>& l);
     private:
         std::size_t m_max_iter;
         double m_rho;
@@ -31,15 +31,16 @@ namespace scopi{
     };
 
     template<class projection_t>
-    nesterov<projection_t>::nesterov(std::size_t max_iter, double rho, double tol_dg, double tol_l)
+    projected_gradient<projection_t>::projected_gradient(std::size_t max_iter, double rho, double tol_dg, double tol_l)
     : projection_t()
     , m_max_iter(max_iter)
     , m_rho(rho)
     , m_tol_dg(tol_dg)
     , m_tol_l(tol_l)
+    {}
 
     template<class projection_t>
-    int nesterov<projection_t>::projection(const sparse_matrix_t& A, const struct matrix_descr& descr, const xt::xtensor<double, 1>& c, xt::xtensor<double, 1>& l)
+    std::size_t projected_gradient<projection_t>::projection(const sparse_matrix_t& A, const struct matrix_descr& descr, const xt::xtensor<double, 1>& c, xt::xtensor<double, 1>& l)
     {
         std::size_t iter = 0;
         double theta_old = 1.;
@@ -47,21 +48,21 @@ namespace scopi{
         m_l_old = l;
         while (iter < m_max_iter)
         {
-            // DG = Ay+c
-            m_dg = c;
+            // dg = A*y+c
+            xt::noalias(m_dg) = c;
             m_status = mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1., A, descr, m_y.data(), 1., m_dg.data());
             PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_d_mv for dg = A*y+dg: " << m_status;
 
             xt::noalias(l) = this->projection_cone(m_y - m_rho * m_dg);
             double theta = 0.5*(theta_old + std::sqrt(4.+theta_old*theta_old) - theta_old*theta_old);
-            double beta = theta_old*(1.-theta_old)/(theta_old*theta_old + theta);
-            xt::noalias(m_y) = l + beta*(l - m_l_old);
-            double norm_dg = xt::norm_linf(dg);
-            double norm_l = xt::norm_linf(l);
+            double beta = theta_old*(1. - theta_old)/(theta_old*theta_old + theta);
+            m_y = l + beta*(l - m_l_old);
+            double norm_dg = xt::amax(xt::abs(m_dg))(0);
+            double norm_l = xt::amax(xt::abs(l))(0);
 
             // uu = A*l + c
             // TODO for performance reasons, do that only if verbose mode
-            m_uu = m_c
+            xt::noalias(m_uu) = c;
             m_status = mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1., A, descr, l.data(), 1., m_uu.data());
             PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_d_mv for uu = A*l+c: " << m_status;
             double constraint = double((xt::amin(m_uu))(0));
@@ -76,6 +77,8 @@ namespace scopi{
             theta_old = theta;
             iter++;
         }
+        PLOG_ERROR << "Uzawa does not converge";
+        return iter;
     }
 }
 #endif
