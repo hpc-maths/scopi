45429eb05668d584774dc1694f479c63ffe9138d
helene.bloch <helene.bloch@polytechnique.edu>
2022-06-23 14:18:05 +0200

    use functions

--- include/scopi/solvers/OptimProjectedGradient.hpp
+++ include/scopi/solvers/OptimProjectedGradient.hpp
@@ -102,6 +102,11 @@ namespace scopi{
                                std::vector<MKL_INT>& invP_csr_col,
                                std::vector<double>& invP_csr_val,
                                const scopi_container<3>& particles);
+        template <std::size_t dim>
+        void create_matrix_B(const scopi_container<dim>& particles,
+                             const std::vector<neighbor<dim>>& contacts,
+                             problem_t& problem);
+        void create_matrix_A();
 
         xt::xtensor<double, 1> m_l;
         xt::xtensor<double, 1> m_e; // vector c in 220517_PbDual_MiniForces.pdf
@@ -160,12 +165,8 @@ namespace scopi{
 
         m_status = mkl_sparse_set_mv_hint(m_inv_P, SPARSE_OPERATION_NON_TRANSPOSE, m_descr_inv_P, 2 );
         PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mv_hint for matrix invP: " << m_status;
-
-        /*
-        m_status = mkl_sparse_set_mm_hint( m_inv_P, SPARSE_OPERATION_NON_TRANSPOSE, m_descr_inv_P, SPARSE_LAYOUT_ROW_MAJOR, 0, 1);
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mm_hint for matrix invP: " << m_status;
-        */
-
+        // m_status = mkl_sparse_set_mm_hint( m_inv_P, SPARSE_OPERATION_NON_TRANSPOSE, m_descr_inv_P, SPARSE_LAYOUT_ROW_MAJOR, 0, 1);
+        // PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mm_hint for matrix invP: " << m_status;
         m_status = mkl_sparse_optimize ( m_inv_P );
         PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_optimize for matrix invP: " << m_status;
     }
@@ -177,102 +178,28 @@ namespace scopi{
                                                                                               problem_t& problem)
     {
         xt::noalias(m_l) = xt::zeros<double>({problem.number_row_matrix(contacts)});
-
-        // u = P^{-1}*c
+        // u = P^{-1}*c = vap
         m_status = mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1., m_inv_P, m_descr_inv_P, this->m_c.data(), 0., m_u.data());
         PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_d_mv for u = P^{-1}*c: " << m_status;
 
-        // matrix B
-        problem.create_matrix_constraint_coo(particles, contacts, 0);
-        sparse_matrix_t B_coo;
-        m_descrB.type = SPARSE_MATRIX_TYPE_GENERAL;
+        create_matrix_B(particles, contacts, problem);
+        create_matrix_A();
 
-        m_status =  mkl_sparse_d_create_coo(&B_coo,
-                                           SPARSE_INDEX_BASE_ZERO,
-                                           problem.number_row_matrix(contacts), // number of rows
-                                           6*this->m_nparts, // number of cols
-                                           problem.m_A_values.size(), // number of non-zero elements
-                                           problem.m_A_rows.data(),
-                                           problem.m_A_cols.data(),
-                                           problem.m_A_values.data());
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_d_create_coo for matrix B: " << m_status;
-
-        m_status = mkl_sparse_convert_csr(B_coo,
-                                          SPARSE_OPERATION_NON_TRANSPOSE,
-                                          &m_B);
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_convert_csr for matrix B: " << m_status;
-
-        m_status = mkl_sparse_destroy ( B_coo );
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_destroy for matrix B_coo: " << m_status;
-
-        m_status = mkl_sparse_order(m_B);
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_order for matrix B: " << m_status;
-
-        m_status = mkl_sparse_set_mv_hint(m_B, SPARSE_OPERATION_NON_TRANSPOSE, m_descrB, 1 );
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mv_hint for matrix B SPARSE_OPERATION_NON_TRANSPOSE: " << m_status;
-
-        m_status = mkl_sparse_set_mv_hint(m_B, SPARSE_OPERATION_TRANSPOSE, m_descrB, 1 );
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mv_hint for matrix B SPARSE_OPERATION_TRANSPOSE: " << m_status;
-
-        /*
-        m_status = mkl_sparse_set_mm_hint(m_B, SPARSE_OPERATION_NON_TRANSPOSE, m_descrB, SPARSE_LAYOUT_ROW_MAJOR, 0, 1);
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mm_hint for matrix B SPARSE_OPERATION_NON_TRANSPOSE: " << m_status;
-
-        m_status = mkl_sparse_set_mm_hint(m_B, SPARSE_OPERATION_TRANSPOSE, m_descrB, SPARSE_LAYOUT_ROW_MAJOR, 0, 1);
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mm_hint for matrix B SPARSE_OPERATION_TRANSPOSE: " << m_status;
-        */
-
-        m_status = mkl_sparse_optimize ( m_B );
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_optimize for matrix B: " << m_status;
-
-        // tmp = P^{-1}*B
-        sparse_matrix_t tmp;
-
-        m_descr_inv_P.type = SPARSE_MATRIX_TYPE_GENERAL;
-        m_status = mkl_sparse_sp2m(SPARSE_OPERATION_NON_TRANSPOSE, m_descr_inv_P, m_inv_P,
-                SPARSE_OPERATION_TRANSPOSE, m_descrB, m_B,
-                SPARSE_STAGE_FULL_MULT, &tmp);
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_sp2m for tmp = P^{-1}*B^T: " << m_status;
-        m_descr_inv_P.type = SPARSE_MATRIX_TYPE_DIAGONAL;
-        m_descr_inv_P.diag = SPARSE_DIAG_NON_UNIT;
-
-        m_status = mkl_sparse_order(tmp);
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_order for matrix tmp: " << m_status;
-
-        // A = B^T*tmp
-        m_status = mkl_sparse_sp2m(SPARSE_OPERATION_NON_TRANSPOSE, m_descrB, m_B,
-                SPARSE_OPERATION_NON_TRANSPOSE, m_descrB, tmp,
-                SPARSE_STAGE_FULL_MULT, &m_A);
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_sp2m for A = B*tmp: " << m_status;
-        m_descrA.type = SPARSE_MATRIX_TYPE_GENERAL;
-
-        m_status = mkl_sparse_destroy ( tmp );
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_destroy for matrix tmp: " << m_status;
-
-        m_status = mkl_sparse_set_mv_hint(m_A, SPARSE_OPERATION_NON_TRANSPOSE, m_descrA, 2 );
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mv_hint for matrix A: " << m_status;
-
-        m_status = mkl_sparse_optimize ( m_A );
-        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_optimize for matrix A: " << m_status;
-
-        // e = B*v^{a priori}+d
+        // e = -B*u+distances
         xt::noalias(m_e) = problem.m_distances;
-
         m_status = mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, -1., m_B, m_descrB, m_u.data(), 1., m_e.data());
         PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_d_mv for e = B*u+d: " << m_status;
 
         std::size_t nb_iter = this->projection(m_A, m_descrA, m_e, m_l);
 
-        // u = vap + P^{-1}*B^T*l
+        // u = u - P^{-1}*B^T*l
         m_status = mkl_sparse_d_mv(SPARSE_OPERATION_TRANSPOSE, 1., m_B, m_descrB, m_l.data(), 0., m_bl.data());
         PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_d_mv for bl = B^T*l: " << m_status;
-
         m_status = mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, -1., m_inv_P, m_descr_inv_P, m_bl.data(), 1., m_u.data());
         PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_d_mv for u = vap - P^{-1}*bl: " << m_status;
 
         m_status = mkl_sparse_destroy ( m_B );
         PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_destroy for matrix B: " << m_status;
-
         m_status = mkl_sparse_destroy ( m_A );
         PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_destroy for matrix A: " << m_status;
 
@@ -301,6 +228,71 @@ namespace scopi{
     int OptimProjectedGradient<problem_t, gradient_t>::get_nb_active_contacts_impl() const
     {
         return xt::sum(xt::where(m_l > 0., xt::ones_like(m_l), xt::zeros_like(m_l)))();
+    }
+
+    template <class problem_t, class gradient_t>
+    template <std::size_t dim>
+    void OptimProjectedGradient<problem_t, gradient_t>::create_matrix_B(const scopi_container<dim>& particles,
+                                                                        const std::vector<neighbor<dim>>& contacts,
+                                                                        problem_t& problem)
+    {
+        m_descrB.type = SPARSE_MATRIX_TYPE_GENERAL;
+        sparse_matrix_t B_coo;
+
+        problem.create_matrix_constraint_coo(particles, contacts, 0);
+        m_status =  mkl_sparse_d_create_coo(&B_coo,
+                                           SPARSE_INDEX_BASE_ZERO,
+                                           problem.number_row_matrix(contacts), // number of rows
+                                           6*this->m_nparts, // number of cols
+                                           problem.m_A_values.size(), // number of non-zero elements
+                                           problem.m_A_rows.data(),
+                                           problem.m_A_cols.data(),
+                                           problem.m_A_values.data());
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_d_create_coo for matrix B: " << m_status;
+        m_status = mkl_sparse_convert_csr(B_coo, SPARSE_OPERATION_NON_TRANSPOSE, &m_B);
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_convert_csr for matrix B: " << m_status;
+        m_status = mkl_sparse_destroy ( B_coo );
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_destroy for matrix B_coo: " << m_status;
+        m_status = mkl_sparse_order(m_B);
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_order for matrix B: " << m_status;
+        m_status = mkl_sparse_set_mv_hint(m_B, SPARSE_OPERATION_NON_TRANSPOSE, m_descrB, 1 );
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mv_hint for matrix B SPARSE_OPERATION_NON_TRANSPOSE: " << m_status;
+        m_status = mkl_sparse_set_mv_hint(m_B, SPARSE_OPERATION_TRANSPOSE, m_descrB, 1 );
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mv_hint for matrix B SPARSE_OPERATION_TRANSPOSE: " << m_status;
+        // m_status = mkl_sparse_set_mm_hint(m_B, SPARSE_OPERATION_NON_TRANSPOSE, m_descrB, SPARSE_LAYOUT_ROW_MAJOR, 0, 1);
+        // PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mm_hint for matrix B SPARSE_OPERATION_NON_TRANSPOSE: " << m_status;
+        // m_status = mkl_sparse_set_mm_hint(m_B, SPARSE_OPERATION_TRANSPOSE, m_descrB, SPARSE_LAYOUT_ROW_MAJOR, 0, 1);
+        // PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mm_hint for matrix B SPARSE_OPERATION_TRANSPOSE: " << m_status;
+        m_status = mkl_sparse_optimize ( m_B );
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_optimize for matrix B: " << m_status;
+    }
+
+    template <class problem_t, class gradient_t>
+    void OptimProjectedGradient<problem_t, gradient_t>::create_matrix_A()
+    {
+        m_descrA.type = SPARSE_MATRIX_TYPE_GENERAL;
+        sparse_matrix_t tmp;
+        // tmp = P^{-1}*B
+        m_descr_inv_P.type = SPARSE_MATRIX_TYPE_GENERAL;
+        m_status = mkl_sparse_sp2m(SPARSE_OPERATION_NON_TRANSPOSE, m_descr_inv_P, m_inv_P,
+                                  SPARSE_OPERATION_TRANSPOSE, m_descrB, m_B,
+                                  SPARSE_STAGE_FULL_MULT, &tmp);
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_sp2m for tmp = P^{-1}*B^T: " << m_status;
+        m_descr_inv_P.type = SPARSE_MATRIX_TYPE_DIAGONAL;
+        m_descr_inv_P.diag = SPARSE_DIAG_NON_UNIT;
+        m_status = mkl_sparse_order(tmp);
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_order for matrix tmp: " << m_status;
+        // A = B^T*tmp
+        m_status = mkl_sparse_sp2m(SPARSE_OPERATION_NON_TRANSPOSE, m_descrB, m_B,
+                                  SPARSE_OPERATION_NON_TRANSPOSE, m_descrB, tmp,
+                                  SPARSE_STAGE_FULL_MULT, &m_A);
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_sp2m for A = B*tmp: " << m_status;
+        m_status = mkl_sparse_destroy ( tmp );
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_destroy for matrix tmp: " << m_status;
+        m_status = mkl_sparse_set_mv_hint(m_A, SPARSE_OPERATION_NON_TRANSPOSE, m_descrA, 2 );
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS && m_status != SPARSE_STATUS_NOT_SUPPORTED) << "Error in mkl_sparse_set_mv_hint for matrix A: " << m_status;
+        m_status = mkl_sparse_optimize ( m_A );
+        PLOG_ERROR_IF(m_status != SPARSE_STATUS_SUCCESS) << "Error in mkl_sparse_optimize for matrix A: " << m_status;
     }
 
     template <class problem_t, class gradient_t>
