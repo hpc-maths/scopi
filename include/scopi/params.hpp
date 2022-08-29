ec73eabad529844f65e122e91104767b2d27f18c
Helene <helenebloch@tutanota.com>
2022-08-29 20:43:11 +0200

    documentation for params.hpp

--- include/scopi/params.hpp
+++ include/scopi/params.hpp
@@ -7,40 +7,103 @@
 
 namespace scopi
 {
+    /**
+     * @brief Parameters for the optimization solver.
+     *
+     * To be specialized.
+     *
+     * @tparam solver_t Type of the solver.
+     */
     template<class solver_t>
     struct OptimParams
     {
     private:
+        /**
+         * @brief Constructor.
+         */
         OptimParams();
     };
 
+    /**
+     * @brief Parameters for the problem.
+     *
+     * To be specialized.
+     *
+     * @tparam problem_t Type of the problem.
+     */
     template<class problem_t>
     struct ProblemParams
     {
     private:
+        /**
+         * @brief Constructor.
+         */
         ProblemParams();
     };
 
+    /**
+     * @brief Parameters for the contacts.
+     *
+     * To be specialized.
+     *
+     * @tparam contact_t Type of the contacts.
+     */
     template<class contact_t>
     struct ContactsParams
     {
     private:
+        /**
+         * @brief Constructor.
+         */
         ContactsParams();
     };
 
+    /**
+     * @brief Parameters for the a priori velocity.
+     *
+     * To be specialized.
+     *
+     * @tparam vap_t Type of the a priori velocity.
+     */
     template<class vap_t>
     struct VapParams
     {
     private:
+        /**
+         * @brief Constructor.
+         */
         VapParams();
     };
 
+    /**
+     * @brief Parameters for the main solver.
+     */
     struct ScopiParams
     {
+        /**
+         * @brief Default constructor.
+         */
         ScopiParams();
+        /**
+         * @brief Copy constructor.
+         *
+         * @param params Parameters to be copied.
+         */
         ScopiParams(const ScopiParams& params);
 
+        /**
+         * @brief Frequency to write the output files.
+         *
+         * Write output files with the current iteration is a multiple of \c output_frequency.
+         * If \c output_frequency is <tt> std::size_t(-1) <tt>, then write the output files at each time steps.
+         * Default value is <tt> std::size_t(-1) <tt>.
+         */
         std::size_t output_frequency;
+        /**
+         * @brief Path to the output files.
+         *
+         * Directories have to exist before running the code.
+         */
         std::string filename;
     };
 
@@ -49,19 +112,50 @@ namespace scopi
     //          class problem_t = DryWithoutFriction,
     //          class contact_t = contact_kdtree,
     //          class vap_t = vap_fixed>
+    /**
+     * @brief Global struct for the parameters.
+     *
+     * @tparam solver_t Type of the optimization solver.
+     * @tparam problem_t Type of the problem (redundant with \c solver_t).
+     * @tparam contact_t Type of contacts.
+     * @tparam vap_t Type of a priori velocity.
+     */
     template<class solver_t,
              class problem_t,
              class contact_t,
              class vap_t>
     struct Params
     {
+        /**
+         * @brief Default constructor.
+         */
         Params();
+        /**
+         * @brief Copy constructor.
+         *
+         * @param params Parameters to be copied.
+         */
         Params(const Params<solver_t, problem_t, contact_t, vap_t>& params);
 
+        /**
+         * @brief Parameters for the optimization solver.
+         */
         OptimParams<solver_t> optim_params;
+        /**
+         * @brief Parameters for the problem.
+         */
         ProblemParams<problem_t> problem_params;
+        /**
+         * @brief Parameters for the contacts.
+         */
         ContactsParams<contact_t> contacts_params;
+        /**
+         * @brief Parameters for the a priori velocity.
+         */
         VapParams<vap_t> vap_params;
+        /**
+         * @brief Parameters for the main solver.
+         */
         ScopiParams scopi_params;
     };
 
