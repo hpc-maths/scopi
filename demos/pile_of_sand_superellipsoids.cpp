814182a7c179b848b8d08d9d2a1c6c135d2c23cb
helene.bloch <helene.bloch@polytechnique.edu>
2022-08-31 15:45:55 +0200

    pile_of_sand_superellipsoids in 2D

--- demos/pile_of_sand_superellipsoids.cpp
+++ demos/pile_of_sand_superellipsoids.cpp
@@ -12,9 +12,9 @@
 #include <scopi/contact/contact_brute_force.hpp>
 
 template <std::size_t dim>
-void add_obstacle(scopi::scopi_container<dim>& particles, double r, std::size_t i, std::size_t k)
+void add_obstacle(scopi::scopi_container<dim>& particles, double r, std::size_t i)
 {
-    scopi::superellipsoid<dim> s({{i*2.*r, -r, k*2.*r}}, {r, r, r}, {1., 1.});
+    scopi::superellipsoid<dim> s({{i*2.*r, -r}}, {r, r}, 1.);
     particles.push_back(s, scopi::property<dim>().deactivate());
 }
 
@@ -26,7 +26,7 @@ int main()
     double PI = xt::numeric_constants<double>::PI;
 
     double dt = 0.01;
-    std::size_t total_it = 1;
+    std::size_t total_it = 100;
     double width_box = 10.;
     std::size_t n = 3; // n^3 spheres
     double g = 1.;
@@ -39,24 +39,17 @@ int main()
     double r = width_box/2./(n+1);
 
     scopi::scopi_container<dim> particles;
-    auto prop = scopi::property<dim>().force({{0., -g, 0.}});
+    auto prop = scopi::property<dim>().force({{0., -g}});
 
     // obstacles
-    for (std::size_t i = 0; i < n; ++i)
-    {
-        for (std::size_t k = 0; k < n; ++k)
-        {
-            add_obstacle(particles, r, -i,    -k   );
-            add_obstacle(particles, r,  i,    -k   );
-            add_obstacle(particles, r, (i+n), -k   );
-            add_obstacle(particles, r, -i,     k   );
-            add_obstacle(particles, r,  i,     k   );
-            add_obstacle(particles, r, (i+n),  k   );
-            add_obstacle(particles, r, -i,    (k+n));
-            add_obstacle(particles, r,  i,    (k+n));
-            add_obstacle(particles, r, (i+n), (k+n));
-        }
-    }
+    scopi::plan<dim> p({{0., 0}}, PI/2.);
+    particles.push_back(p, scopi::property<dim>().deactivate());
+    // for (std::size_t i = 0; i < n; ++i)
+    // {
+    //     add_obstacle(particles, r, -i   );
+    //     add_obstacle(particles, r,  i   );
+    //     add_obstacle(particles, r, (i+n));
+    // }
 
     std::default_random_engine generator;
     std::uniform_real_distribution<double> distrib_m(1., 2.);
@@ -65,12 +58,9 @@ int main()
     {
         for (std::size_t j = 1; j < n; ++j)
         {
-            for (std::size_t k = 0; k < n; ++k)
-            {
-                double m = distrib_m(generator);
-                scopi::superellipsoid<dim> s({{i*2.*r, r + j*2.*r, k*2.*r}}, {r, r, r}, {1., 1.});
-                particles.push_back(s, prop.mass(m).moment_inertia({m*r*r/2., m*r*r/2., m*r*r/2.}));
-            }
+            double m = distrib_m(generator);
+            scopi::superellipsoid<dim> s({{i*2.*2.*r, r + j*2.*r}}, {2.*r, r}, 1.);
+            particles.push_back(s, prop.mass(m).moment_inertia(m*PI/4.*2.*r*r*r*r));
         }
     }
 
@@ -78,12 +68,9 @@ int main()
     double dec_x = 0.5*r;
     for (std::size_t i = 0; i < n; ++i)
     {
-        for (std::size_t k = 0; k < n; ++k)
-        {
-            double m = distrib_m(generator);
-            scopi::superellipsoid<dim> s({{i*2.*r + dec_x, r, k*2.*r}}, {r, r, r}, {1., 1.});
-            particles.push_back(s, prop.mass(m).moment_inertia({m*r*r/2., m*r*r/2., m*r*r/2.}));
-        }
+        double m = distrib_m(generator);
+        scopi::superellipsoid<dim> s({{i*2.*2.*r + dec_x, r}}, {2.*r, r}, 1.);
+        particles.push_back(s, prop.mass(m).moment_inertia(m*PI/4.*2.*r*r*r*r));
     }
 
     scopi::ScopiSolver<dim, scopi::OptimProjectedGradient<scopi::DryWithoutFriction>, scopi::contact_brute_force, scopi::vap_fpd> solver(particles, dt, params);
