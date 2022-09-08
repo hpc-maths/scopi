d8d1b5d907bc7ccb1237b1346ded8c78d7034a2c
helene.bloch <helene.bloch@polytechnique.edu>
2022-09-08 09:41:18 +0200

    box_spheres_3d with more spheres

--- demos/box_spheres_3d.cpp
+++ demos/box_spheres_3d.cpp
@@ -7,37 +7,39 @@
 #include <scopi/property.hpp>
 
 #include <scopi/solvers/OptimProjectedGradient.hpp>
-#include <scopi/solvers/gradient/nesterov_dynrho_restart.hpp>
+#include <scopi/solvers/gradient/nesterov_restart.hpp>
 #include <scopi/solvers/OptimMosek.hpp>
 #include <scopi/vap/vap_fpd.hpp>
 #include <scopi/contact/contact_brute_force.hpp>
 
 int main()
 {
-    plog::init(plog::info, "box_spheres_3d_new_friction_small_01_convexe.log");
+    plog::init(plog::info, "box_spheres_3d_very_large_nesterov_restart.log");
 
     constexpr std::size_t dim = 3;
     double PI = xt::numeric_constants<double>::PI;
 
-    std::size_t total_it = 500;
+    std::size_t total_it = 100;
     double width_box = 10.;
-    std::size_t n = 8; // n^3 spheres
+    std::size_t n = 50; // n^3 spheres
     double g = 1.;
 
     double r0 = width_box/n/2.;
-    double dt = 0.2*(r0-0.1)/(2.*g);
+    double dt = 0.2*0.9*r0/(2.*g);
     double rho = 0.2/(dt*dt);
     std::cout << "dt = " << dt << "  rho = " << rho << std::endl;
 
-    // scopi::Params<scopi::OptimProjectedGradient<scopi::DryWithoutFriction, scopi::nesterov_dynrho_restart<>>, scopi::DryWithoutFriction, scopi::contact_brute_force, scopi::vap_fpd> params;
-    // params.scopi_params.output_frequency = 2000;
-    // params.optim_params.tol_l = 1e-3;
-    // params.optim_params.rho = rho;
+    scopi::Params<scopi::OptimProjectedGradient<scopi::DryWithoutFriction, scopi::nesterov_restart<>>, scopi::DryWithoutFriction, scopi::contact_kdtree, scopi::vap_fpd> params;
+    params.scopi_params.output_frequency = 99;
+    params.optim_params.tol_l = 1e-3;
+    params.optim_params.rho = rho;
+    params.contacts_params.dmax = 0.9*r0;
+    params.contacts_params.kd_tree_radius = params.contacts_params.dmax + 2.*0.9*r0;
 
-    scopi::Params<scopi::OptimMosek<scopi::DryWithFriction>, scopi::DryWithFriction, scopi::contact_brute_force, scopi::vap_fpd> params;
-    params.optim_params.change_default_tol_mosek = false;
-    params.problem_params.mu = 0.1;
-    params.contacts_params.dmax = 4.*(r0-0.1);
+    // scopi::Params<scopi::OptimMosek<scopi::DryWithFriction>, scopi::DryWithFriction, scopi::contact_brute_force, scopi::vap_fpd> params;
+    // params.optim_params.change_default_tol_mosek = false;
+    // params.problem_params.mu = 1.;
+    // params.contacts_params.dmax = 4.*(r0-0.1);
 
     scopi::scopi_container<dim> particles;
     auto prop = scopi::property<dim>().force({{0., -g, 0.}});
@@ -57,7 +59,7 @@ int main()
     particles.push_back(p_back, scopi::property<dim>().deactivate());
 
     std::default_random_engine generator;
-    std::uniform_real_distribution<double> distrib_r(r0-0.2, r0-0.1);
+    std::uniform_real_distribution<double> distrib_r(0.8*r0, 0.9*r0);
     std::uniform_real_distribution<double> distrib_m(1., 2.);
     std::uniform_real_distribution<double> distrib_mouve_center(-0.05, 0.05);
 
@@ -78,8 +80,8 @@ int main()
         }
     }
 
-    scopi::ScopiSolver<dim, scopi::OptimMosek<scopi::DryWithFriction>, scopi::contact_brute_force, scopi::vap_fpd> solver(particles, dt, params);
-    // scopi::ScopiSolver<dim, scopi::OptimProjectedGradient<scopi::DryWithoutFriction, scopi::nesterov_dynrho_restart<>>, scopi::contact_brute_force, scopi::vap_fpd> solver(particles, dt, params);
+    // scopi::ScopiSolver<dim, scopi::OptimMosek<scopi::DryWithFriction>, scopi::contact_brute_force, scopi::vap_fpd> solver(particles, dt, params);
+    scopi::ScopiSolver<dim, scopi::OptimProjectedGradient<scopi::DryWithoutFriction, scopi::nesterov_restart<>>, scopi::contact_kdtree, scopi::vap_fpd> solver(particles, dt, params);
     solver.solve(total_it);
 
     return 0;
