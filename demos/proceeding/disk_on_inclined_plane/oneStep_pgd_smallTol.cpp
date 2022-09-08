5905efa2f4c854e671c222e2d603c72c83a56a45
helene.bloch <helene.bloch@polytechnique.edu>
2022-09-08 10:43:43 +0200

    comment for disk_on_inclined_plane/oneStep_pgd_smallTol.cpp

--- demos/proceeding/disk_on_inclined_plane/oneStep_pgd_smallTol.cpp
+++ demos/proceeding/disk_on_inclined_plane/oneStep_pgd_smallTol.cpp
@@ -11,7 +11,10 @@
 
 int main()
 {
-    plog::init(plog::info, "disk_on_inclined_plane_oneStep_pdg_smallTol.log");
+    // Figure 2: Disk placed on a plane without friction. Evolution of the constraint and cost as a function of the iterations for the PGD algorithm.
+    // Table 1: Disk placed on a plane without friction. Number of iterations for the PGD algorithm and tolerance 10^{-9}.
+    // The constraint is the first column in the log, the cost is the second column.
+    plog::init(plog::verbose, "disk_on_inclined_plane_oneStep_pdg_smallTol.log");
 
     constexpr std::size_t dim = 2;
     double PI = xt::numeric_constants<double>::PI;
@@ -24,6 +27,7 @@ int main()
     scopi::Params<scopi::OptimProjectedGradient<scopi::DryWithoutFriction, scopi::uzawa<>>, scopi::DryWithoutFriction, scopi::contact_kdtree, scopi::vap_fpd> params;
     params.optim_params.tol_l = 1e-9;
     params.optim_params.rho = 2.;
+    params.optim_params.verbose = true;
 
     double dt = 0.05;
     std::size_t total_it = 1;
