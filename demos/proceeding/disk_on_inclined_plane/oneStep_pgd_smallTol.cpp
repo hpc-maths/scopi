a449a28f92f52332c589a0e9a3c362b3815f600e
Helene <helenebloch@tutanota.com>
2022-09-08 10:27:34 +0200

    add a test for disk on inclined plane, one step, pgd, small tolerance

---
+++ demos/proceeding/disk_on_inclined_plane/oneStep_pgd_smallTol.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <xtensor/xmath.hpp>
+#include <scopi/objects/types/sphere.hpp>
+#include <scopi/objects/types/plan.hpp>
+#include <scopi/solver.hpp>
+#include <scopi/property.hpp>
+
+#include <scopi/solvers/OptimProjectedGradient.hpp>
+#include <scopi/solvers/gradient/uzawa.hpp>
+#include <scopi/vap/vap_fpd.hpp>
+
+int main()
+{
+    plog::init(plog::info, "disk_on_inclined_plane_oneStep_pdg_smallTol.log");
+
+    constexpr std::size_t dim = 2;
+    double PI = xt::numeric_constants<double>::PI;
+
+    double radius = 1.;
+    double g = 1.;
+    double mass = 1.;
+    double h = radius;
+    auto prop = scopi::property<dim>().mass(mass).moment_inertia(mass*radius*radius/2.);
+    scopi::Params<scopi::OptimProjectedGradient<scopi::DryWithoutFriction, scopi::uzawa<>>, scopi::DryWithoutFriction, scopi::contact_kdtree, scopi::vap_fpd> params;
+    params.optim_params.tol_l = 1e-9;
+    params.optim_params.rho = 2.;
+
+    double dt = 0.05;
+    std::size_t total_it = 1;
+    double alpha = PI/6.;
+
+    scopi::scopi_container<dim> particles;
+    scopi::plan<dim> p({{0., 0.}}, PI/2.-alpha);
+    scopi::sphere<dim> s({{h*std::sin(alpha), h*std::cos(alpha)}}, radius);
+    particles.push_back(p, scopi::property<dim>().deactivate());
+    particles.push_back(s, prop.force({{0., -g}}));
+
+    auto solver(particles, dt, params);
+    solver.solve(total_it);
+
+    return 0;
+}
