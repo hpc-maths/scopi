a167d686712bb6c35343e0914cdb772964244486
Helene <helenebloch@tutanota.com>
2022-09-09 18:18:20 +0200

    update comment for spheres_in_box_small_config_*

--- demos/proceeding/multiparticles_tests/spheres_in_box_small_config_apgdas.cpp
+++ demos/proceeding/multiparticles_tests/spheres_in_box_small_config_apgdas.cpp
@@ -12,7 +12,7 @@
 
 int main()
 {
-    // Figure 8 and table 3: spheres falling in a box with APGD-AS solver.
+    // Figure 8 and table 3: spheres falling in a box with APGD-AS solver and 8^3 spheres.
     plog::init(plog::info, "spheres_in_box_small_config_apgdas.log");
 
     constexpr std::size_t dim = 3;
