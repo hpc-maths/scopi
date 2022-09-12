d3b509750cfdddc0dc42d7ab85bebe3ab5ab8303
Helene <helenebloch@tutanota.com>
2022-09-12 14:03:00 +0200

    Add documentation for worm.hpp

--- include/scopi/objects/types/worm.hpp
+++ include/scopi/objects/types/worm.hpp
@@ -24,30 +24,101 @@ namespace scopi
         return p;
     }
 
+    /**
+     * @brief Worm.
+     *
+     * A worm is a collection of spheres that are in contact (the distance between two successive spheres is 0 instead of being positive).
+     * All spheres have the same radius.
+     *
+     * @tparam dim Dimension (2 or 3).
+     * @tparam owner
+     */
     template<std::size_t dim, bool owner=true>
     class worm: public object<dim, owner>
     {
     public:
 
+        /**
+         * @brief Alias for the base class \c object.
+         */
         using base_type = object<dim, owner>;
+        /**
+         * @brief Alias for position type.
+         */
         using position_type = typename base_type::position_type;
+        /**
+         * @brief Alias for quaternion type.
+         */
         using quaternion_type = typename base_type::quaternion_type;
 
+        /**
+         * @brief Constructor with default rotation.
+         *
+         * Successive spheres should be in contact.
+         *
+         * @param pos [in] Array of positions of centers of the spheres.
+         * @param radius [in] Radius of the spheres. All spheres have the same radius.
+         * @param size [in] Number of spheres in the worm.
+         */
         worm(position_type pos, double radius, std::size_t size);
+        /**
+         * @brief Constructor with given rotation for the spheres.
+         *
+         * Successive spheres should be in contact.
+         *
+         * @param pos [in] Array of positions of centers of the spheres.
+         * @param q [in] Array of quaternions describing the rotation of the spheres.
+         * @param radius [in] Radius of the spheres. All spheres have the same radius.
+         * @param size [in] Number of spheres in the worm.
+         */
         worm(position_type pos, quaternion_type q, double radius, std::size_t size);
 
+        /**
+         * @brief 
+         *
+         * TODO
+         *
+         * @return 
+         */
         virtual std::unique_ptr<base_constructor<dim>> construct() const override;
+        /**
+         * @brief Print the elements of the worm on standard output.
+         */
         virtual void print() const override;
+        /**
+         * @brief Get the hash of the worm.
+         */
         virtual std::size_t hash() const override;
 
+        /**
+         * @brief Get the radius of the spheres in the worm.
+         */
         double radius() const;
+        /**
+         * @brief Get a sphere in the worm.
+         *
+         * @param i [in] Index of the sphere in the worm. 0 <= i < this->size.
+         *
+         * @return Pointer to the i-th sphere in the worm.
+         */
         std::unique_ptr<object<dim, false>> get_sphere(std::size_t i) const;
 
     private:
 
+        /**
+         * @brief Create the hash of the worm.
+         *
+         * Two worms with the same dimension, same radius and same number of spheres have the same hash. 
+         */
         void create_hash();
 
+        /**
+         * @brief Radius of the spheres in the worm.
+         */
         double m_radius;
+        /**
+         * @brief Hash of the worm.
+         */
         std::size_t m_hash;
     };
 
