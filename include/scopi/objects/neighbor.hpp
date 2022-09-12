0d6461131e8c8b3921abbb5dcb63ebbb9012a3be
Helene <helenebloch@tutanota.com>
2022-09-12 10:45:44 +0200

    start adding documentation for neighbor.hpp

--- include/scopi/objects/neighbor.hpp
+++ include/scopi/objects/neighbor.hpp
@@ -5,15 +5,54 @@
 
 namespace scopi
 {
+    /**
+     * @brief Structure of a neighbor.
+     *
+     * Particles \c i and \c j are in contact.
+     * \c pi = \c pj if the two particles are in contact.
+     *
+     * @tparam dim Dimension (2 or 3).
+     */
     template<std::size_t dim>
     struct neighbor
     {
-        std::size_t i, j;
+        /**
+         * @brief Index of the particle \c i.
+         */
+        std::size_t i;
+        /**
+         * @brief Index of the particle \c j.
+         */
+        std::size_t j;
+        /**
+         * @brief Distance between particles \c i and \c j;
+         */
         double dij;
+        /**
+         * @brief Outer normal vector to particle \c j.
+         */
         xt::xtensor_fixed<double, xt::xshape<dim>> nij;
-        xt::xtensor_fixed<double, xt::xshape<dim>> pi, pj;
+        /**
+         * @brief Point in particle \c i which realizes the distance between the two particles.
+         */
+        xt::xtensor_fixed<double, xt::xshape<dim>> pi;
+        /**
+         * @brief Point in particle \c j which realizes the distance between the two particles.
+         */
+        xt::xtensor_fixed<double, xt::xshape<dim>> pj;
     };
 
+    /**
+     * @brief 
+     *
+     * TODO
+     *
+     * @tparam dim
+     * @param out
+     * @param neigh
+     *
+     * @return 
+     */
     template<std::size_t dim>
     std::ostream& operator<<(std::ostream& out, const neighbor<dim>& neigh)
     {
