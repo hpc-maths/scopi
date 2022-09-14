635d0fd01e90c5dd70cf4cb5c93203960a42ea0c
Helene <helenebloch@tutanota.com>
2022-09-14 21:01:12 +0200

    solvers pgd, etc: remove duplication l_old and lamba_prev

--- include/scopi/solvers/gradient/apgd.hpp
+++ include/scopi/solvers/gradient/apgd.hpp
@@ -100,10 +100,6 @@ namespace scopi{
          * @brief Vector \f$ \l^{\indexUzawa} \f$.
          */
         xt::xtensor<double, 1> m_l_old;
-        /**
-         * @brief Vector \f$ \l^{\indexUzawa} \f$.
-         */
-        xt::xtensor<double, 1> m_lambda_prev;
     };
 
     template<class problem_t>
@@ -123,10 +119,9 @@ namespace scopi{
         std::size_t iter = 0;
         double theta_old = 1.;
         m_y = l;
-        m_l_old = l;
         while (iter < m_max_iter)
         {
-            xt::noalias(m_lambda_prev) = l;
+            xt::noalias(m_l_old) = l;
             // dg = A*y+c
             xt::noalias(m_dg) = c;
             m_status = mkl_sparse_d_mv(SPARSE_OPERATION_NON_TRANSPOSE, 1., A, descr, m_y.data(), 1., m_dg.data());
@@ -139,7 +134,7 @@ namespace scopi{
             // double norm_dg = xt::amax(xt::abs(m_dg))(0);
             // double norm_l = xt::amax(xt::abs(l))(0);
             // double cmax = double((xt::amin(m_dg))(0));
-            double diff_lambda = xt::amax(xt::abs(l - m_lambda_prev))(0) / (xt::amax(xt::abs(m_lambda_prev))(0) + 1.);
+            double diff_lambda = xt::amax(xt::abs(l - m_l_old))(0) / (xt::amax(xt::abs(m_l_old))(0) + 1.);
 
             if (m_verbose)
             {
@@ -161,7 +156,6 @@ namespace scopi{
                 return iter+1;
             }
 
-            m_l_old = l;
             theta_old = theta;
             iter++;
         }
