2cacaaccad8cea31a752d53bdd53b7f664733c5e
Loic Gouarin <loic.gouarin@gmail.com>
2024-06-26 08:10:20 +0200

    fix cppcheck

--- test/utils.hpp
+++ test/utils.hpp
@@ -13,7 +13,7 @@ namespace scopi
 {
     static constexpr double PI        = xt::numeric_constants<double>::PI;
     static constexpr double tolerance = 1e-6;
-    bool diffFile(const fs::path filenameRef, const std::filesystem::path filenameResult, double tol = 1e-12);
+    bool diffFile(const fs::path filenameResult, const std::filesystem::path filenameRef, double tol = 1e-12);
     bool check_reference_file(const std::filesystem::path path, const std::string_view filename, std::size_t it, double tolerance = 1e-12);
     std::pair<type::position_t<2>, double> analytical_solution_sphere_plane(double alpha, double mu, double t, double r, double g, double y0);
     std::pair<type::position_t<2>, double>
